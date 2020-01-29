5c6195ad349b6d75cc04c3462e6a32c0f687db14
유경진 <@DESKTOP-EAF22F2>
2020-01-29 21:09:34 +0900

    Main C3P0Data 추가

--- src/native_jdbc/Main.java
+++ src/native_jdbc/Main.java
@@ -3,6 +3,7 @@ package native_jdbc;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import native_jdbc.ds.C3P0DataSource;
 import native_jdbc.ds.DBCPDataSource;
 import native_jdbc.ds.Hikari_DataSource;
 import native_jdbc.ds.Hikari_DataSource2;
@@ -12,7 +13,16 @@ public class Main {
 
 	public static void main(String[] args) {
 //		connection_hikari();
+//		connection_dbcp();
 		
+		try (Connection con = C3P0DataSource.getConnection();){
+			System.out.println(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void connection_dbcp() {
 		try (Connection con = DBCPDataSource.getConnection();){
 			System.out.println(con);
 		} catch (SQLException e) {
